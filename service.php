ea022b862f9f93d6235653fed2e2f77335f5b949
MediavukProbaGit <bonitafcrs@yahoo.com>
2018-12-12 15:58:14 +0100

    12/12/2018

--- service.php
+++ service.php
@@ -2,50 +2,131 @@
 
 	<!-- ====================== START service CONTENT-PAGE ====================== -->
 
-	<div class="service width-80 margin-top-100">
-		<!-- /////// START service__header \\\\\\\ -->
-		<div class="service__header">
-			<div class="service__header__title">
-				<h1>internet marketing</h1>
-			</div>
+	<div class="service-content">
+		<div class="service width-1160 margin-top-100 margin-bottom-100">
+			<!-- /////// START service__header \\\\\\\ -->
+			<div class="service__header">
+				<div class="service__header__title">
+					<h1>internet marketing</h1>
+				</div>
 
-			<div>
-				<img src="img/icons/Internet-marketing_icon.svg" alt="">
-			</div>
-		</div>
-		<!-- /////// END: service__header \\\\\\\ -->
-
-
-
-		<!-- /////// START service__description \\\\\\\ -->
-		<div class="service__description">
-			<div class="description__text">
-				<p class="service-text">
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-				</p>
-			</div>
-
-			<div class="description__box">
-				<div class="box">
-					<a href="">social media & </a> 
-					<a href="">reputation</a><br> 
-					<a href="">seo</a><br>
-					<a href="">mobilni marketing</a><br>
-					<a href="">pay per click</a>
-					<div class="patternContentText"></div>
+				<div>
+					<img src="img/icons/Internet-marketing_icon.svg" alt="">
 				</div>
 			</div>
+			<!-- /////// END: service__header \\\\\\\ -->
 
-			<div class="description__img">
-				
+
+
+			<!-- /////// START service__description \\\\\\\ -->
+			<div class="service__description">
+				<div class="description__text">
+					<p class="service-text">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only.
+					</p>
+				</div>
+
+				<div class="description__box">
+					<div class="box">
+						<a href="">social media & </a> 
+						<a href="">reputation</a><br> 
+						<a href="">seo</a><br>
+						<a href="">mobilni marketing</a><br>
+						<a href="">pay per click</a>
+						<div class="patternContentText"></div>
+					</div>
+				</div>
+
 			</div>
-		</div>
-		<!-- /////// END: service__description \\\\\\\ -->
+			<!-- /////// END: service__description \\\\\\\ -->
 
 
 
-		<div class="service__content">
-			
+			<!-- /////// START service__content \\\\\\\ -->
+			<div class="service__content margin-top-100 margin-bottom-100">
+
+				<div class="reputation margin-bottom-100 service-flex-end">
+
+					<div class="reputation__img margin-bottom-100">
+							<img src="img/2.jpg" alt="">
+					</div> 
+
+					<div class="reputation__title">
+						<h1 class="service-title">social media & reputation</h1>
+					</div>
+
+					<div class="reputation__text-button">
+						<p class="service-text">
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.  
+						</p>
+						<div class="text-button">
+							<a class="service-button" href="">
+								read more
+							</a>
+						</div>
+
+						
+					</div>
+				</div>
+
+
+
+				<div class="reputation margin-bottom-100 service-flex-start">
+					
+					<div class="reputation__img margin-bottom-100">
+							<img src="img/3.jpg" alt="">
+					</div>
+
+					<div class="reputation__title">
+						<h1 class="service-title">search engine optimization</h1>
+					</div>
+
+					<div class="reputation__text-button">
+						<p class="service-text">
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.  
+						</p>
+						<div class="text-button">
+							<a class="service-button" href="">
+								read more
+							</a>
+						</div>
+
+						
+					</div>
+
+				</div>
+
+				
+
+				<div class="reputation margin-bottom-100 service-flex-end">
+
+					<div class="reputation__img margin-bottom-100">
+							<img src="img/1.jpg" alt="">
+					</div>
+
+					<div class="reputation__title">
+						<h1 class="service-title">mobile marketing</h1>
+					</div>
+
+					<div class="reputation__text-button">
+						<p class="service-text">
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.  
+						</p>
+						<div class="text-button">
+							<a class="service-button" href="">
+								read more
+							</a>
+						</div>
+
+						
+					</div>
+
+				</div>
+
+
+			</div>
+			<!-- /////// END: service__content \\\\\\\ -->
+
 		</div>
 	</div>
 
